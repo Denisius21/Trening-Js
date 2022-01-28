6bbb2893aa3723293d8eac72b075206dcd816e2e
Denisius21 <denishka2112@gmail.com>
2022-01-28 15:28:09 +0200

    Add functions

---
+++ index.js
@@ -0,0 +1,67 @@
+/*const funcQ = function(name){
+const usName = prompt('Enter your name');
+const answerUs = prompt('You going?');
+
+
+const resAnswerUs = 'da';
+const resAnswerUs2 = 'no';
+console.log(name);
+
+if(answerUs === resAnswerUs){
+  alert('Good bye '+ usName)
+}
+if(answerUs === resAnswerUs2) {
+  alert('Welcom '+ usName);
+}
+
+}
+funcQ();*/
+
+
+const newFunct = function(param1){
+  numb = param1;
+  if(numb % 2 === 0){
+    return 'true';
+  }
+  if(numb % 2 === 1){
+    return 'false';
+  }
+}
+let userNumber = prompt('Enter any number');
+const result = newFunct(userNumber);
+
+const secondFunct = function(){
+  if(result === 'true'){
+    alert( ':))');
+  }else{
+    alert( '((((');
+  }
+}
+const smail = secondFunct(result);
+console.log(smail);
+
+/*const litleNumber = function(number1,number2){
+  if(number1 > number2){
+    return number2;
+  }else{
+    return number1;
+  }
+}
+let number1 = prompt('Enter any number');
+let number2 = prompt('Enter any number');
+const result = litleNumber(number1,number2);*/
+
+
+/*const silesFunct = function(par1, par2){
+  if(par1 > par2){
+    return 'Спасибо за покупку';
+  }else{
+    return 'На Вашем счету не достаточно средств!'
+  }
+}
+number1 = prompt('Введите кол-во своих средств');
+number2 = prompt('Введите стоимость товара');
+let answer = silesFunct(number1,number2);
+alert(answer);*/
+
+
